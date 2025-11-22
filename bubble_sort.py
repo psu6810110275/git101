a07a2f7e9ddda671e0e202501917c977b1cbef28
Manattee Vilairat <manattee2998@gmail.com>
2025-11-22 23:45:58 +0700

    create bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,7 @@
+def bubble_sort(numbers):
+    pass #write your code here
+
+if __name__ == "__main__":
+    numbers = list(map(int, input("Enter integer numbers separated by space: ").split()))
+    sorted_numbers = bubble_sort(numbers)
+    print("Sorted numbers are", sorted_numbers)
